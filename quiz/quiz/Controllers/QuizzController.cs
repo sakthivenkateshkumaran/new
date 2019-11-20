044d7b39b2d46026343e15cdbee4237e2fff17b7
sakthivenkateshkumaran <sakthi2K17ksrcas@gmail.com>
2019-11-20 15:15:58 +0530

    message

---
+++ quiz/quiz/Controllers/QuizzController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System. 
+namespace quiz.Controllers
+{
+    public class QuizzController : Controller
+    {
+        // GET: Quizz
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult GetUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetUser(UserVM user)
+        {
+            UserVM userConnected = dbContext.Users.Where(u => u.FullName == user.FullName)
+                                         .Select(u => new UserVM
+                                         {
+                                             UserID = u.UserID,
+                                             FullName = u.FullName,
+                                             ProfilImage = u.ProfilImage,
+
+                                         }).FirstOrDefault();
+
+            if (userConnected != null)
+            {
+                Session["UserConnected"] = userConnected;
+                return RedirectToAction("SelectQuizz");
+            }
+            else
+            {
+                ViewBag.Msg = "Sorry : user is not found !!";
+                return View();
+            }
+
+        }
+    }
+}
